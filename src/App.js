95b49a16d37e002d439c2451e033f20ccd190708
Your Name <you@example.com>
2023-02-15 19:12:18 -0800

    route DOM

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css';
 import { useState, useRef } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
@@ -5,44 +6,35 @@ import { DataContext } from './context/DataContext'
 import { SearchContext } from './context/SearchContext'
 
 function App() {
-    let [message, setMessage] = useState('Search for Music!')
- let [data, setData] = useState([])
- let searchInput = useRef('')
+  let [data, setData] = useState([])
+  let [message, setMessage] = useState('Search for Music!')
+  let searchInput = useRef('')
 
-    const API_URL = 'https://itunes.apple.com/search?term='
-    
-    const handleSearch = (e, term) => {
-        e.preventDefault()
-        // Fetch Data
-        const fetchData = async () => {
-            document.title = `${term} Music`
-            const response = await fetch(API_URL + term)
-            const resData = await response.json()
-            if (resData.results.length > 0) {
-                // Set State and Context value
-                return setData(resData.results)
-            } else {
-                return setMessage('Not Found')
-            }
-        }
-        fetchData()
-    }
-    
-    return (
-        <div className="App">
-            <SearchContext.Provider value={{
-                term: searchInput,
-                handleSearch: handleSearch
-                }}>
-                <SearchBar />
-            </SearchContext.Provider>
-            {message}
-            <DataContext.Provider value={data}>
-                <Gallery />
-            </DataContext.Provider>
-        </div>
-    );
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    fetch(`https://itunes.apple.com/search?term=${term}`)
+    .then(response => response.json())
+    .then(resData => {
+      if (resData.results.length > 0) {
+        return setData(resData.results)
+      } else {
+        return setMessage('Not Found.')
+      }
+    })
+    .catch(err => setMessage('An Error has Occurred!'))
+  }
+
+  return (
+    <div className="App">
+      <SearchContext.Provider value={{term: searchInput, handleSearch: handleSearch}}>
+        <SearchBar />
+      </SearchContext.Provider>
+      {message}
+      <DataContext.Provider value={data}>
+        <Gallery />
+      </DataContext.Provider>
+    </div>
+  );
 }
 
 export default App;
-
